a917dd926e9aa252c5ee1bbc3266c2a92f846db3
Rodrigo Sakae <devsakae@gmail.com>
2024-07-12 23:34:05 -0300

    chore: about me

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ export default function About() {
     <div className="max-w-6xl mx-auto p-4 space-y-4">
       <h1 className="text-2xl font-medium text-amber-500">About me</h1>
       <p>Passionate by tech, music and sports, I never care too much about movies and films, yet I do like to watch comedy, action and sometimes drama.</p>
-      <p>I live in Brazil, and now I'm directing my career to be a web developer.</p>
+      <p>Get to know me more @ <a href="http://devsakae.vercel.app">devsakae.vercel.app</a></p>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     <div className='flex justify-between mx-4 p-4 max-w-6xl sm:mx-auto items-center py-6'>
       <div className='flex'>
         <MenuItem title={'MOVIES'} address={'/'} Icon={ BsFilm } />
-        <MenuItem title={'ABOUT'} address={'/about'} Icon={ BsInfoSquare } />
+        <MenuItem title={'ABOUT ME'} address={'/about'} Icon={ BsInfoSquare } />
         <MenuItem title={'CONTACT'} address={'https://b09bz2i5ujx.typeform.com/to/QMUuYZVd'} Icon={ AiOutlineMail } />
       </div>
       <div className='flex items-center space-x-5 p-x-4'>
@@ -17,7 +17,7 @@ export default function Header() {
         <Link href="/">
           <h2 className='text-2xl'>
             <span className='font-bold text-2xl mx-1'>DS</span>
-            <span className='font-bold bg-amber-500 py-1 px-2 rounded-lg'>MD</span>
+            <span className='font-bold bg-amber-500 py-1 px-2 rounded-lg dark:text-black'>MD</span>
           </h2>
         </Link>
       </div>
